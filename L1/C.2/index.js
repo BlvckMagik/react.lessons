c4e66527ed1dccc42d159cf274804ab9ee21e65b
BlvckMagik <dandol777@gmail.com>
2021-09-26 09:08:33 +0300

    less

---
+++ L1/C.2/index.js
@@ -0,0 +1,15 @@
+const rootEl = document.querySelector('#root');
+
+const greetingEl = React.createElement(
+  'div',
+  { className: 'greeting' },
+
+  React.createElement('div', { className: 'greeting__title' }, 'Hello, world!'),
+  React.createElement(
+    'div',
+    { className: 'greeting__text' },
+    "I'm learning React"
+  )
+);
+
+ReactDOM.render(greetingEl, rootEl);
