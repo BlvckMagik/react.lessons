39829eb1a364ef0e345205f93a8eea2c811e079d
BlvckMagik <dandol777@gmail.com>
2021-10-03 18:36:50 +0300

    less

---
+++ L3/C.3/src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Comment from './Comment.jsx';
+import './styles.scss';
+
+const userInfo = {
+  name: 'Tom',
+  avatarUrl: 'https://avatars.githubusercontent.com/u/82097854?v=4',
+};
+
+const rootEl = document.querySelector('#root');
+
+ReactDOM.render(
+  <Comment user={userInfo} text='Good job!' date={new Date()} />,
+  rootEl
+);
