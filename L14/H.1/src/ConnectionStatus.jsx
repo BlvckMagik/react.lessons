d1feb4c7cf6d4f55e68790e540b60bc6d9c987e4
BlvckMagik <dandol777@gmail.com>
2021-10-27 17:55:40 +0300

    less

---
+++ L14/H.1/src/ConnectionStatus.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+
+const ConnectionStatus = () => {
+  const [status, onStatusChange] = useState('online');
+
+  useEffect(() => {
+    const onlineEvent = () => {
+      onStatusChange('online');
+    };
+
+    const offlineEvent = () => {
+      onStatusChange('offline');
+    };
+    window.addEventListener('online', onlineEvent);
+    window.addEventListener('offline', offlineEvent);
+    return () => {
+      window.removeEventListener('online', onlineEvent);
+      window.removeEventListener('offline', offlineEvent);
+    };
+  }, [status]);
+
+  return status === 'online' ? (
+    <div className='status'>{status}</div>
+  ) : (
+    <div className='status status_offline'>{status}</div>
+  );
+};
+
+export default ConnectionStatus;
